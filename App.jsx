b9fceb8c57a9f33ef12a2e5cb436aa1c89687929
maxxx234 <meanzzz38@gmail.com>
2024-09-14 16:15:06 +0530

    hrllo

--- App.jsx
+++ App.jsx
@@ -8,6 +8,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
 return (
   <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
     <div className=' fixed  top-0 -z-10 h-full w-full'> 
